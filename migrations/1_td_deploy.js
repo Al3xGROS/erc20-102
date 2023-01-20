d58dab1f1a0c89b10d93ea97ea789d5d2400e9ee
Al3xGROS <al.gros@sfr.fr>
2023-01-20 19:17:08 +0100

    Calling_another_contract_from_your_contract

--- migrations/1_td_deploy.js
+++ migrations/1_td_deploy.js
@@ -1,29 +1,37 @@
-
 var TDErc20 = artifacts.require("ERC20TD.sol");
 var ERC20Claimable = artifacts.require("ERC20Claimable.sol");
 var evaluator = artifacts.require("Evaluator.sol");
+var solution = artifacts.require("ExerciceSolution.sol");
 
 
 module.exports = (deployer, network, accounts) => {
     deployer.then(async () => {
-        await deployTDToken(deployer, network, accounts); 
-        await deployEvaluator(deployer, network, accounts); 
-        await setPermissionsAndRandomValues(deployer, network, accounts); 
+        // await deployTDToken(deployer, network, accounts); 
+        // await deployEvaluator(deployer, network, accounts); 
+        // await setPermissionsAndRandomValues(deployer, network, accounts); 
+        await setStaticContracts(deployer, network, accounts); 
         await deployRecap(deployer, network, accounts); 
+        await testSolution(deployer, network, accounts);
     });
 };
 
-async function deployTDToken(deployer, network, accounts) {
-	TDToken = await TDErc20.new("TD-ERC20-101","TD-ERC20-101",web3.utils.toBN("20000000000000000000000000000"))
-	ClaimableToken = await ERC20Claimable.new("ClaimableToken","CLTK",web3.utils.toBN("20000000000000000000000000000"))
-}
+// async function deployTDToken(deployer, network, accounts) {
+// 	TDToken = await TDErc20.new("TD-ERC20-102-23","TD-ERC20-102-23",web3.utils.toBN("20000000000000000000000000000"))
+// 	ClaimableToken = await ERC20Claimable.new("ClaimableToken23","CLTK-23",web3.utils.toBN("20000000000000000000000000000"))
+// }
 
-async function deployEvaluator(deployer, network, accounts) {
-	Evaluator = await evaluator.new(TDToken.address, ClaimableToken.address)
-}
+// async function deployEvaluator(deployer, network, accounts) {
+// 	Evaluator = await evaluator.new(TDToken.address, ClaimableToken.address)
+// }
 
-async function setPermissionsAndRandomValues(deployer, network, accounts) {
-	await TDToken.setTeacher(Evaluator.address, true)
+// async function setPermissionsAndRandomValues(deployer, network, accounts) {
+// 	await TDToken.setTeacher(Evaluator.address, true)
+// }
+
+async function setStaticContracts(deployer, network, accounts) {
+	TDToken = await TDErc20.at("0xb79a94500EE323f15d76fF963CcE27cA3C9e32DF")
+	ClaimableToken = await ERC20Claimable.at("0xE70AE39bDaB3c3Df5225E03032D31301E2E71B6b")
+	Evaluator = await evaluator.at("0x16F3F705825294A55d40D3D34BAF9F91722d6143")
 }
 
 async function deployRecap(deployer, network, accounts) {
@@ -32,4 +40,21 @@ async function deployRecap(deployer, network, accounts) {
 	console.log("Evaluator " + Evaluator.address)
 }
 
-
+async function testSolution(deployer, network, accounts) {
+	mySolution = await solution.new(ClaimableToken.address)
+	console.log("ContractAddress " + mySolution.address)
+    console.log("MyAccount " + accounts[0])
+	await Evaluator.submitExercice(mySolution.address)
+	myBalance = await TDToken.balanceOf(accounts[0])
+	console.log("Balance|Submit " + myBalance)
+	await Evaluator.ex2_claimedFromContract()
+	myBalance = await TDToken.balanceOf(accounts[0])
+	console.log("Balance|Ex1 " + myBalance)
+	await Evaluator.ex3_withdrawFromContract()
+	myBalance = await TDToken.balanceOf(accounts[0])
+	console.log("Balance|Ex2 " + myBalance)
+    // await Evaluator.ex4_approvedExerciceSolution()
+    // myBalance = await TDToken.balanceOf(accounts[0])
+	// console.log("my balance " + myBalance)
+	console.log()
+}
